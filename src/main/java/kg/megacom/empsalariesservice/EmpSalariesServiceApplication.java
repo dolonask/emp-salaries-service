57367efa7e1cb03c0f2951a57c581debf2cd6bbd
Dolon Askarbekov <dolon_aa@mail.ru>
2021-07-01 19:29:30 +0600

    Initial commit

---
+++ src/main/java/kg/megacom/empsalariesservice/EmpSalariesServiceApplication.java
@@ -0,0 +1,13 @@
+package kg.megacom.empsalariesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpSalariesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpSalariesServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/megacom/empsalariesservice/EmpSalariesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.megacom.empsalariesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpSalariesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
